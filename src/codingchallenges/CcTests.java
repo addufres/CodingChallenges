2655037a4da0a7c6837023ae0c5e9c684778bde3
AndrewD <adufresplatinum@gmail.com>
2018-06-27 13:57:26 -0600

    fibIterative

--- src/codingchallenges/CcTests.java
+++ src/codingchallenges/CcTests.java
@@ -7,13 +7,14 @@ public class CcTests {
 	private static FizzBuzz fb = new FizzBuzz();
 	private static StringReverse sr = new StringReverse();
 	private static IsUnique iu = new IsUnique();
+	private static FibIterative fi = new FibIterative();
 	public static void main(String[] args) {
-		sv.swapValues(3,5);
-		fb.fizzBuzz();
-     	iu.getUnique("23456378"); // false
-  		iu.getUnique("abcdefghij2345"); // true
-		sr.reverse("hello");
-		
+//		sv.swapValues(3,5); // 5,3
+//		fb.fizzBuzz(); // not writing that here
+//     	iu.getUnique("23456378"); // false
+//  	iu.getUnique("abcdefghij2345"); // true
+//		sr.reverse("hello"); // "olleh"
+//		fi.fibIt(); // not writing that here
 	}
 	
 }
--- src/codingchallenges/FibIterative.java
+++ src/codingchallenges/FibIterative.java
@@ -3,4 +3,41 @@ package codingchallenges;
 public class FibIterative {
 //	Fibonacci(iterative)
 //	Print the first 20 numbers of the fibonacci sequence.
+	private int dblPrev = 0;
+	private int prev = 1;
+	private int current = 0;
+	public int getDblPrev() {
+		return dblPrev;
+	}
+	public void setDblPrev(int dblPrev) {
+		this.dblPrev = dblPrev;
+	}
+	public int getPrev() {
+		return prev;
+	}
+	public void setPrev(int prev) {
+		this.prev = prev;
+	}
+	public int getCurrent() {
+		return current;
+	}
+	public void setCurrent(int current) {
+		this.current = current;
+	}
+	public FibIterative(int dblPrev, int prev, int current) {
+		super();
+		this.dblPrev = dblPrev;
+		this.prev = prev;
+		this.current = current;
+	}
+	public FibIterative() {}
+	public void fibIt() {
+		for (int i = 2; i <= 21; i++)
+	    {
+	        current = prev + dblPrev;
+	        dblPrev = prev;
+	        prev = current;
+	        System.out.println(current);
+	    }
+	}
 }
